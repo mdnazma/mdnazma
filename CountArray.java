df369f5859284f53b779aca8e3fdb438e3978fbd
nazma <mdnazma4@gmail.com>
2021-12-06 21:08:18 +0530

    task 1

---
+++ CountArray.java
@@ -0,0 +1,21 @@
+
+public class CountArray {
+
+	static void isexists(int arr[],element) {
+		int count=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==element) {
+				count=count+1;
+			}
+				System.out.println("The repeated elements count is"+count);
+				System.out.println("The repeated element is"+a[i] );
+				
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner sc = new Scanner.in(System.in);
+	}
+
+}
